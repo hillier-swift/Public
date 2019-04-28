81ea75ea1ee3af6f75ce92492f3a972c090693e0
tonyhillierswift <tony.hillier-swift@novasystems.com>
2019-04-28 15:23:23 +0100

    Testing Blog intergation

---
+++ Ping-Subnet.ps1
@@ -0,0 +1,9 @@
+﻿#usage Ping-range.ps1 -subnet 10.14.0
+param
+([parameter(Mandatory=$true)][string]$subnet,
+[string]$outfile = "C:\Stuff\PingRange.csv"
+)
+
+$results = 1..254 | foreach { new-object psobject -prop @{Address=”$subnet.$_”;Ping=(test-connection “$subnet.$_” -quiet -count 1)}}
+
+$results | Export-csv $outfile
